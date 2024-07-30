b0c3f00eadf5e67ec937a55c020d3b6f5d5bab53
Sebastián <sebastian090295@gmail.com>
2024-07-29 21:13:38 -0300

    project 02-ta-te-ti

---
+++ 02-ta-te-ti/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import confetti from "canvas-confetti";
+import { Square } from "./components/Square.jsx";
+import { TURNS } from "./constants.js";
+import { checkWinnerFrom, checkEndGame } from "./logic/board.js";
+import { WinnerModal } from "./components/WinnerModal.jsx";
+import { saveGameToStorage, resetGameStorage } from "./logic/storage/index.js";
+
+function App() {
+    const [board, setBoard] = useState(() => {
+        const boardFromStorage = window.localStorage.getItem("board");
+
+        return boardFromStorage
+            ? JSON.parse(boardFromStorage)
+            : Array(9).fill(null);
+    });
+
+    const [turn, setTurn] = useState(() => {
+        const turnFromStorage = window.localStorage.getItem("turn");
+        return turnFromStorage ?? TURNS.X;
+    });
+
+    const [winner, setWinner] = useState(null);
+    //null no hay ganador, false hay un empate
+
+    const resetGame = () => {
+        setBoard(Array(9).fill(null));
+        setTurn(TURNS.X);
+        setWinner(null);
+        resetGameStorage();
+    };
+
+    const updateBoard = (index) => {
+        //Para no sobreescribir la posición
+        if (board[index] || winner) return;
+        //actualizar el tablero
+        const newBoard = [...board];
+        newBoard[index] = turn;
+        setBoard(newBoard);
+        //cambiar el turno
+
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+        setTurn(newTurn);
+
+        //guardar aquí la partida
+
+        saveGameToStorage({
+            board: newBoard,
+            turn: newTurn,
+        });
+
+        //revisar si hay un ganador
+        const newWinner = checkWinnerFrom(newBoard);
+        if (newWinner) {
+            setWinner(newWinner);
+            confetti();
+        } else if (checkEndGame(newBoard)) setWinner(false);
+    };
+
+    return (
+        <main className="board">
+            <h1>Ta-te-ti</h1>
+            <button onClick={resetGame}>Reset del juego</button>
+            <section className="game">
+                {board.map((square, index) => {
+                    return (
+                        <Square
+                            key={index}
+                            index={index}
+                            updateBoard={updateBoard}
+                        >
+                            {square}
+                        </Square>
+                    );
+                })}
+            </section>
+
+            <section className="turn">
+                <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+                <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+            </section>
+
+            <WinnerModal resetGame={resetGame} winner={winner} />
+        </main>
+    );
+}
+
+export default App;
---
+++ 02-ta-te-ti/src/logic/board.js
@@ -0,0 +1,28 @@
+import { WINNER_COMBOS } from "../constants.js";
+
+export const checkWinnerFrom = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras
+    //para ver si x u o ganó
+    for (const combo of WINNER_COMBOS) {
+        const [a, b, c] = combo;
+        if (
+            boardToCheck[a] &&
+            boardToCheck[a] === boardToCheck[b] &&
+            boardToCheck[a] === boardToCheck[c]
+        ) {
+            return boardToCheck[a];
+        }
+    }
+    //si no hay ganador
+    return null;
+};
+
+export const checkEndGame = (newBoard) => {
+    //revisamos si hay un empate
+    //si no hay mas espacios vacíos
+    //en el tablero
+
+    return newBoard.every((square) => square !== null);
+    //si todas las posiciones (square) son diferentes a null (vacias)
+    //ha terminado el juego
+};
